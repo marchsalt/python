55cae75be85769657dec092d6b13df547d3a42b6
marchsalt <ad7211@mail.ru>
2026-03-19 13:19:20 +0300

    somethink

---
+++ sem 2/practice.py
@@ -0,0 +1,10 @@
+abc=int(input())
+a=abc//100
+b =(abc//10)%10
+c=abc%10
+print(a,b,c)
+print(a,c,b)
+print(b,a,c)
+print(b,c,a)
+print(c,a,b)
+print(c,b,a)
---
+++ sem6/task.py
@@ -0,0 +1,62 @@
+#st = str(input())
+#print(st[::2])
+
+
+#first_NAME = input()
+#second_NAME = input()
+#third_NAME = input()
+#inc= first_NAME[0]+second_NAME[0]+third_NAME[0]
+#print(inc.upper())
+
+
+#st=input()
+#res=0
+#for i in st:
+#    res=int(i)
+#print(res)
+
+
+#st=input()
+#plus=0
+#star=0
+#for i in st:
+#    if i == "+":
+#        plus += 1
+#    if i == "*":
+#        star += 1
+#print(f'символ + встречается {plus}раз\nСимвол * встречается {star} раз')
+
+
+# st=input()
+# last=''
+# counter = 0
+# for i in st:
+#     if i == last:
+#         counter += 1
+#     last = i
+# print(counter)
+
+
+# word=input()
+# drow=word[::-1]
+# if drow==word:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# st=input()
+# #
+# # print(len(st))
+# # print(st*3)
+# # print(st[0])
+# # print(st[0:2])
+# # print(st[::-1])
+# # print(st[1:-1])
+#
+# print(st[2])
+# print(st[])
+
+
+
+
